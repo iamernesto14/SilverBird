24aba973e0fae2d3a261d9907aa049d5b36dedff
iamernesto14 <eernesto211@gmail.com>
2025-08-02 16:55:34 +0000

    feat: Add sidebar functionalities

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  activeItem: string = 'home';
 
+  setActive(item: string) {
+    this.activeItem = item;
+  }
 }
